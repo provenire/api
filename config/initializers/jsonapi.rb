8b5ff06f7b2a03a2644361aad6388812c4047c9a
Miles Zimmerman <miles@maztek.io>
2016-04-14 18:01:58 -0700

    Change serialization to use dashes in attribute names

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -1,6 +1,6 @@
 JSONAPI.configure do |config|
   # :underscored_key, :camelized_key, :dasherized_key, or custom
-  config.json_key_format = :underscored_key
+  config.json_key_format = :dasherized_key
 
   # :underscored_route, :camelized_route, :dasherized_route, or custom
   config.route_format = :underscored_route
